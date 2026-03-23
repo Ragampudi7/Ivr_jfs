f464d94645bb834fc4c95b5143c05c6e2c08ec0b
Siva <Sivasatyasai05@gmail.com>
2026-03-23 13:57:36 +0530

    Configure application.properties for Aiven MySQL and support dotenv

--- src/main/java/com/ivr/Application.java
+++ src/main/java/com/ivr/Application.java
@@ -4,11 +4,16 @@ package com.ivr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
+        // Load .env variables into System Properties so Spring Boot placeholder ${} injection works!
+        // We use ignoreIfMissing() so that it doesn't crash when running on Render where .env file is omitted.
+        Dotenv.configure().ignoreIfMissing().systemProperties().load();
+        
         SpringApplication.run(Application.class, args);
     }
 }
